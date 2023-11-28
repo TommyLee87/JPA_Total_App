fc142d375ce6f7ae64cebf0084d43ad15481cb0d
TommyLee <kokokorkr@naver.com>
2023-11-28 10:38:12 +0900

    jwt reflesh 231128

--- src/main/java/com/kh/jpatotalapp/jwt/TokenProvider.java
+++ src/main/java/com/kh/jpatotalapp/jwt/TokenProvider.java
@@ -1,23 +1,28 @@
 package com.kh.jpatotalapp.jwt;
-
 import com.kh.jpatotalapp.dto.TokenDto;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
-
 import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
 import java.util.stream.Collectors;
 
-
-@Component
 @Slf4j
-
+@Component
 // 토큰 생성, 토큰 검증, 토큰에서 회원 정보 추출
 public class TokenProvider {
     private static final String AUTHORITIES_KEY = "auth"; // 토큰에 저장되는 권한 정보의 key
@@ -30,6 +35,7 @@ public class TokenProvider {
     public TokenProvider(@Value("${jwt.secret}") String secretKey) {
         this.key = Keys.secretKeyFor(SignatureAlgorithm.HS512); // HS512 알고리즘을 사용하는 키 생성
     }
+
     // 토큰 생성
     public TokenDto generateTokenDto(Authentication authentication) {
         // 권한 정보 문자열 생성,
@@ -65,11 +71,49 @@ public class TokenProvider {
                 .refreshTokenExpiresIn(refreshTokenExpiresIn.getTime())
                 .build();
     }
+    public Authentication getAuthentication(String accessToken) {
+        // 토큰 복호화
+        Claims claims = parseClaims(accessToken);
 
+        // 토큰 복호화에 실패하면
+        if (claims.get(AUTHORITIES_KEY) == null) {
+            throw new RuntimeException("권한 정보가 없는 토큰입니다.");
+        }
 
+        // 토큰에 담긴 권한 정보들을 가져옴
+        Collection<? extends GrantedAuthority> authorities =
+                Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
 
+        // 권한 정보들을 이용해 유저 객체를 만들어서 반환, 여기서 User 객체는 UserDetails 인터페이스를 구현한 객체
+        User principal = new User(claims.getSubject(), "", authorities);
+
+        // 유저 객체, 토큰, 권한 정보들을 이용해 인증 객체를 생성해서 반환
+        return new UsernamePasswordAuthenticationToken(principal, accessToken, authorities);
+    }
+    // 토큰의 유효성 검증
+    public boolean validateToken(String token) {
+        try {
+            io.jsonwebtoken.Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (SecurityException | io.jsonwebtoken.MalformedJwtException e) {
+            log.info("잘못된 JWT 서명입니다.");
+        } catch (ExpiredJwtException e) {
+            log.info("만료된 JWT 토큰입니다.");
+        } catch (UnsupportedJwtException e) {
+            log.info("지원되지 않는 JWT 토큰입니다.");
+        } catch (IllegalArgumentException e) {
+            log.info("JWT 토큰이 잘못되었습니다.");
+        }
+        return false;
+    }
+    // 토큰 복호화
+    private Claims parseClaims(String accessToken) {
+        try {
+            return io.jsonwebtoken.Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(accessToken).getBody();
+        } catch (ExpiredJwtException e) {
+            return e.getClaims();
+        }
+    }
 }
-
-
-
-
